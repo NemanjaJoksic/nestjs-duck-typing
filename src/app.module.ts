b77c68bf4e93613fe76c74d3432abe6df47c37ef
Nemanja Joksic <joksicnemanja994@gmail.com>
2024-02-11 19:44:07 +0100

    remove unnecessary modules from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common'
-import { DatabaseModule } from './database/module'
 import { ControllerModule } from './controllers/module'
-import { ServiceModule } from './services/module'
-import { ValidatorModule } from './validations/module'
 
 @Module({
-  imports: [DatabaseModule, ValidatorModule, ServiceModule, ControllerModule],
+  imports: [ControllerModule],
 })
 export class AppModule {}
